ac3fe7b1e1f78c5c74473d9c1c3b415fdbe85d09
Cole Brown <git@spydez.com>
2019-11-18 10:40:31 -0800

    Another unit test implemented for spotify-json.el
    Added spotify-ert/spotify--json-internal-status-field test.

--- spotify-json-ert.el
+++ spotify-json-ert.el
@@ -108,83 +108,66 @@
 (ert-deftest spotify-ert/spotify--json-encode-internal ()
   "Test for encoding data to our json-internal format."
 
-  (let ((str-shrunk (spotify--json-encode-internal
-                     "Aesop Rock" "Shrunk"
-                     265333 9 t t nil))
-        (json-shrunk nil)
-        (json-shrunk-roundtrip nil)
-        (str-foil (spotify--json-encode-internal
-                   "\"Weird Al\" Yankovic" "Foil"
-                   11111 99 nil nil t))
-        (json-foil nil)
-        (json-foil-roundtrip nil))
+  ;; Build our four hash-table json structures for testing.
+  ;; Two via `spotify--json-encode-internal' and two from test data.
+  (spotify-ert/data/with-json-internal
 
-    ;; Convert back to json so we can compare better...
-    ;; JSON doesn't have an ordering, so no guarentee...
-    (spotify--json-setup
-     ;; back to json for these
-     (setq json-shrunk-roundtrip (json-read-from-string str-shrunk))
-     (setq json-foil-roundtrip (json-read-from-string str-foil))
-     ;; test data string to json for these, compare aganist our round trips.
-     (setq json-shrunk (json-read-from-string
-                        (nth 0 spotify-ert/data-string/json-internal)))
-     (setq json-foil (json-read-from-string
-                      (nth 1 spotify-ert/data-string/json-internal))))
+   ;;---
+   ;; Test "Aesop Rock"
+   ;;---
+   (should (string= (gethash 'artist json-shrunk)
+                    (gethash 'artist json-shrunk-roundtrip)))
 
-    ;;---
-    ;; Test "Aesop Rock"
-    ;;---
-    (should (string= (gethash 'artist json-shrunk)
-                     (gethash 'artist json-shrunk-roundtrip)))
+   (should (string= (gethash 'name json-shrunk)
+                    (gethash 'name json-shrunk-roundtrip)))
 
-    (should (string= (gethash 'name json-shrunk)
-                     (gethash 'name json-shrunk-roundtrip)))
+   (should (string= (gethash 'player_state json-shrunk)
+                    (gethash 'player_state json-shrunk-roundtrip)))
 
-    (should (string= (gethash 'player_state json-shrunk)
-                     (gethash 'player_state json-shrunk-roundtrip)))
+   (should (= (gethash 'duration json-shrunk)
+              (gethash 'duration json-shrunk-roundtrip)))
 
-    (should (= (gethash 'duration json-shrunk)
-               (gethash 'duration json-shrunk-roundtrip)))
+   (should (= (gethash 'track_number json-shrunk)
+              (gethash 'track_number json-shrunk-roundtrip)))
 
-    (should (= (gethash 'track_number json-shrunk)
-               (gethash 'track_number json-shrunk-roundtrip)))
+   (should (eq (gethash 'player_shuffling json-shrunk)
+               (gethash 'player_shuffling json-shrunk-roundtrip)))
 
-    (should (eq (gethash 'player_shuffling json-shrunk)
-                (gethash 'player_shuffling json-shrunk-roundtrip)))
+   (should (eq (gethash 'player_repeating json-shrunk)
+               (gethash 'player_repeating json-shrunk-roundtrip)))
 
-    (should (eq (gethash 'player_repeating json-shrunk)
-                (gethash 'player_repeating json-shrunk-roundtrip)))
+   ;;---
+   ;; Test "\"Weird Al\""
+   ;;---
+   (should (string= (gethash 'artist json-foil)
+                    (gethash 'artist json-foil-roundtrip)))
+   ;; This was a problem with our hand-crafted json -
+   ;; Weird Al's quotation marks.
+   ;; So make sure we do not have backslashes and do have quotes.
+   (should-not (string-match "\\\\" (gethash 'artist json-foil)))
+   (should-not (string-match "\\\\" (gethash 'artist json-foil-roundtrip)))
+   (should (string-match "\"" (gethash 'artist json-foil)))
+   (should (string-match "\"" (gethash 'artist json-foil-roundtrip)))
+   (should (string-match "\"Weird Al\"" (gethash 'artist json-foil)))
+   (should (string-match "\"Weird Al\"" (gethash 'artist json-foil-roundtrip)))
 
-    ;;---
-    ;; Test "\"Weird Al\""
-    ;;---
-    (should (string= (gethash 'artist json-foil)
-                     (gethash 'artist json-foil-roundtrip)))
-    ;; This was a problem with our hand-crafted json -
-    ;; Weird Al's quotation marks.
-    ;; So make sure we do not have backslashes and do have quotes.
-    (should-not (string-match "\\\\" (gethash 'artist json-foil)))
-    (should-not (string-match "\\\\" (gethash 'artist json-foil-roundtrip)))
-    (should (string-match "\"" (gethash 'artist json-foil)))
-    (should (string-match "\"" (gethash 'artist json-foil-roundtrip)))
+   (should (string= (gethash 'name json-foil)
+                    (gethash 'name json-foil-roundtrip)))
 
-    (should (string= (gethash 'name json-foil)
-                     (gethash 'name json-foil-roundtrip)))
+   (should (string= (gethash 'player_state json-foil)
+                    (gethash 'player_state json-foil-roundtrip)))
 
-    (should (string= (gethash 'player_state json-foil)
-                     (gethash 'player_state json-foil-roundtrip)))
+   (should (= (gethash 'duration json-foil)
+              (gethash 'duration json-foil-roundtrip)))
 
-    (should (= (gethash 'duration json-foil)
-               (gethash 'duration json-foil-roundtrip)))
+   (should (= (gethash 'track_number json-foil)
+              (gethash 'track_number json-foil-roundtrip)))
 
-    (should (= (gethash 'track_number json-foil)
-               (gethash 'track_number json-foil-roundtrip)))
+   (should (eq (gethash 'player_shuffling json-foil)
+               (gethash 'player_shuffling json-foil-roundtrip)))
 
-    (should (eq (gethash 'player_shuffling json-foil)
-                (gethash 'player_shuffling json-foil-roundtrip)))
-
-    (should (eq (gethash 'player_repeating json-foil)
-                (gethash 'player_repeating json-foil-roundtrip)))))
+   (should (eq (gethash 'player_repeating json-foil)
+               (gethash 'player_repeating json-foil-roundtrip)))))
 
 
 ;;------------------------------------------------------------------------------
@@ -192,7 +175,103 @@
 ;;------------------------------------------------------------------------------
 
 ;; (defun spotify--json-internal-status-field (status field) ...
+(ert-deftest spotify-ert/spotify--json-internal-status-field ()
+  "Test for encoding data to our json-internal format."
 
+  ;; Build four hash-table json structures for testing.
+  ;; Two via `spotify--json-encode-internal' and two from test data.
+  (spotify-ert/data/with-json-internal
+
+   ;;---
+   ;; Aesop Rock - Shrunk
+   ;;---
+
+   ;; compare by hand
+   (should (string= "Aesop Rock"
+                    (spotify--json-internal-status-field json-shrunk :artist)))
+   (should (string= "Shrunk"
+                    (spotify--json-internal-status-field json-shrunk :track)))
+   (should (= 9
+              (spotify--json-internal-status-field json-shrunk :track-number)))
+   (should (= 265333
+              (spotify--json-internal-status-field json-shrunk
+                                                   :duration-millisecond)))
+   (should (eq t (spotify--json-internal-status-field json-shrunk
+                                                      :playing-bool)))
+   (should (eq t (spotify--json-internal-status-field json-shrunk
+                                                      :shuffling-bool)))
+   (should (eq nil (spotify--json-internal-status-field json-shrunk
+                                                        :repeating-bool)))
+
+   ;; compare to each other
+   (should (string= (spotify--json-internal-status-field json-shrunk-roundtrip :artist)
+                    (spotify--json-internal-status-field json-shrunk :artist)))
+   (should (string= (spotify--json-internal-status-field json-shrunk-roundtrip :track)
+                    (spotify--json-internal-status-field json-shrunk :track)))
+   (should (= (spotify--json-internal-status-field json-shrunk-roundtrip :track-number)
+              (spotify--json-internal-status-field json-shrunk :track-number)))
+   (should (= (spotify--json-internal-status-field json-shrunk-roundtrip
+                                                   :duration-millisecond)
+              (spotify--json-internal-status-field json-shrunk
+                                                   :duration-millisecond)))
+   (should (eq (spotify--json-internal-status-field json-shrunk-roundtrip
+                                                    :playing-bool)
+               (spotify--json-internal-status-field json-shrunk
+                                                    :playing-bool)))
+   (should (eq (spotify--json-internal-status-field json-shrunk-roundtrip
+                                                    :shuffling-bool)
+               (spotify--json-internal-status-field json-shrunk
+                                                    :shuffling-bool)))
+   (should (eq (spotify--json-internal-status-field json-shrunk-roundtrip
+                                                    :repeating-bool)
+               (spotify--json-internal-status-field json-shrunk
+                                                    :repeating-bool)))
+
+   ;;---
+   ;; "Weird Al" Yankovic - Foil
+   ;;---
+
+   ;; compare by hand
+   (should (string= "\"Weird Al\" Yankovic"
+                    (spotify--json-internal-status-field json-foil :artist)))
+   (should (string= "Foil"
+                    (spotify--json-internal-status-field json-foil :track)))
+   (should (= 99
+              (spotify--json-internal-status-field json-foil :track-number)))
+   (should (= 11111
+              (spotify--json-internal-status-field json-foil
+                                                   :duration-millisecond)))
+   (should (eq nil (spotify--json-internal-status-field json-foil
+                                                        :playing-bool)))
+   (should (eq nil (spotify--json-internal-status-field json-foil
+                                                        :shuffling-bool)))
+   (should (eq t (spotify--json-internal-status-field json-foil
+                                                      :repeating-bool)))
+
+   ;; compare to each other
+   (should (string= (spotify--json-internal-status-field json-foil-roundtrip :artist)
+                    (spotify--json-internal-status-field json-foil :artist)))
+   (should (string= (spotify--json-internal-status-field json-foil-roundtrip :track)
+                    (spotify--json-internal-status-field json-foil :track)))
+   (should (= (spotify--json-internal-status-field json-foil-roundtrip :track-number)
+              (spotify--json-internal-status-field json-foil :track-number)))
+   (should (= (spotify--json-internal-status-field json-foil-roundtrip
+                                                   :duration-millisecond)
+              (spotify--json-internal-status-field json-foil
+                                                   :duration-millisecond)))
+   (should (eq (spotify--json-internal-status-field json-foil-roundtrip
+                                                    :playing-bool)
+               (spotify--json-internal-status-field json-foil
+                                                    :playing-bool)))
+   (should (eq (spotify--json-internal-status-field json-foil-roundtrip
+                                                    :shuffling-bool)
+               (spotify--json-internal-status-field json-foil
+                                                    :shuffling-bool)))
+   (should (eq (spotify--json-internal-status-field json-foil-roundtrip
+                                                    :repeating-bool)
+               (spotify--json-internal-status-field json-foil
+                                                    :repeating-bool)))
+   ))
 
 ;;------------------------------------------------------------------------------
 ;; Test: spotify--json-api-to-internal
@@ -200,15 +279,76 @@
 
 ;; (defun spotify--json-api-to-internal (player-status) ...
 
+;; §-TODO-§ [2019-11-18]: Impl or skip due to "deprecate-this-plz" TODO?
+
+
+;;   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;;  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;;------------------------------------------------------------------------------
+;;                             END OF UNIT TESTS!
+;;------------------------------------------------------------------------------
+;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;;  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+;;   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 
 ;;------------------------------------------------------------------------------
-;; Test Data
+;; Test Data Helpers
 ;;------------------------------------------------------------------------------
 
 (defun spotify-ert/aggressive-trim (string)
   "Trims a lot of whitespace. All of it."
   (replace-regexp-in-string (rx whitespace) "" string))
 
+
+(defmacro spotify-ert/data/with-json-internal (&rest body)
+  "Creates 4 JSON hash table objects - 2 each for the
+player-statuses in `spotify-ert/data-string/json-internal' with a let binding,
+then executes BODY forms in a progn.
+
+JSON variables:
+  - Aesop Rock
+    - `json-shrunk':
+      - directly from `spotify-ert/data-string/json-internal'
+    - `json-shrunk-roundtrip':
+      - encoded with `spotify--json-encode-internal', then decoded.
+  - \"Weird Al\" Yankovic
+    - `json-foil':
+      - directly from `spotify-ert/data-string/json-internal'
+    - `json-foil-roundtrip':
+      - encoded with `spotify--json-encode-internal', then decoded.
+"
+  `(let ((str-shrunk (spotify--json-encode-internal
+                      "Aesop Rock" "Shrunk"
+                      265333 9 t t nil))
+         (json-shrunk nil)
+         (json-shrunk-roundtrip nil)
+         (str-foil (spotify--json-encode-internal
+                    "\"Weird Al\" Yankovic" "Foil"
+                    11111 99 nil nil t))
+         (json-foil nil)
+         (json-foil-roundtrip nil))
+
+     ;; Convert back to json so we can compare better...
+     ;; JSON doesn't have an ordering, so no guarentee...
+     (spotify--json-setup
+       ;; back to json for these
+       (setq json-shrunk-roundtrip (json-read-from-string str-shrunk))
+       (setq json-foil-roundtrip (json-read-from-string str-foil))
+       ;; test data string to json for these, compare aganist our round trips.
+       (setq json-shrunk (json-read-from-string
+                          (nth 0 spotify-ert/data-string/json-internal)))
+       (setq json-foil (json-read-from-string
+                        (nth 1 spotify-ert/data-string/json-internal))))
+
+     (progn
+       ,@body)))
+
+
+;;------------------------------------------------------------------------------
+;; Test Data
+;;------------------------------------------------------------------------------
 
 (defconst spotify-ert/data-string/json-internal
   '("{
